81611f7151b96135b7d7dc6364e0d28372b967c2
Michael Shumakov <62724405+m-shum@users.noreply.github.com>
2025-07-31 09:37:03 -0700

    fix: declare typescript langtype in playground script setup
    fixes missing typescript errors for useAsyncStoryblok
    
    fix: declare typescript langtype in playground script setup

--- packages/nuxt/playground/pages/index.vue
+++ packages/nuxt/playground/pages/index.vue
@@ -1,32 +1,21 @@
 <script setup lang="ts">
-/* import type { SbBlokData } from '@storyblok/vue'; */
-
-// const storyblokApi = useStoryblokApi();
-// // Checking custom Flush method
-// storyblokApi.flushCache();
-
-const { story, error } = await useAsyncStoryblok('vue', {
+const { story, error } = await useAsyncStoryblok('home', {
   api: {
     version: 'draft',
-    language: 'en',
-    resolve_relations: 'popular-articles.articles',
-  },
-  bridge: {
-    resolveRelations: 'popular-articles.articles',
   },
 });
 
 if (error.value) {
   throw createError({
     statusCode: error.value.statusCode,
-    statusMessage: error.value.statusMessage
+    statusMessage: error.value.statusMessage,
   });
 }
-
 </script>
 
 <template>
   <div>
+    <pre>{{ story.slug }}</pre>
     <StoryblokComponent v-if="story && !error" :blok="story.content" />
     <div v-else-if="error">Error: {{ error.message }}</div>
   </div>
